06aa9ac7955c209970b24ecbc29f8b0137452a2d
nick wuerz <nawuerz@gmail.com>
2020-09-26 12:16:59 -0500

    extracted home component, added router, added icons to ThreadBox and basic styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,16 @@
 import React from "react";
-import ThreadBox from "./components/ThreadBox";
-import RandoBox from "./components/RandoBox";
-import Header from "./components/Header";
-import threadContents from "./thread-contents.js";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 import "./stylesheets/App.css";
 
 const App = () => {
-	return (
-		<div className="app-container ">
-			<Header />
-			<div className="left"></div>
-			<div className="mid">
-				<div className="threads">
-					{threadContents.map((thread) => {
-						return (
-							<ThreadBox
-								key={thread.id}
-								author={thread.author}
-								body={thread.body}
-								threadTitle={thread.threadTitle}
-							/>
-						);
-					})}
-				</div>
-			</div>
-			<div className="right">
-				<div className="boxes">
-					<RandoBox />
-					<RandoBox />
-					<RandoBox />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <>
+      <Switch>
+        <Route exact path={["/", "/home"]} component={Home} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
--- client/src/components/ThreadBox.jsx
+++ client/src/components/ThreadBox.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import ThumbUpOutlinedIcon from "@material-ui/icons/ThumbUpOutlined";
+import ThumbDownOutlinedIcon from "@material-ui/icons/ThumbDownOutlined";
+import ChatBubbleOutlineTwoToneIcon from "@material-ui/icons/ChatBubbleOutlineTwoTone";
+import "../stylesheets/ThreadBox.css";
 
 const ThreadBox = (props) => {
-	return (
-		<div className="stage">
-			<p>{props.author}</p>
-			<h3>{props.threadTitle}</h3>
-			<p>{props.body}</p>
-		</div>
-	);
+  return (
+    <div className="stage">
+      <p id="authorDiv">
+        <span id="author">{props.author}</span>
+        {props.date}
+      </p>
+      <h3>{props.threadTitle}</h3>
+      <p>{props.body}</p>
+      <div id="iconDiv">
+        <ThumbUpOutlinedIcon fontSize="small" />
+        <span className="threadIcon">{props.upvotes}</span>
+        <ThumbDownOutlinedIcon fontSize="small" />
+        <span className="threadIcon">{props.downvotes}</span>
+        <ChatBubbleOutlineTwoToneIcon fontSize="small" />
+        <span>{props.comments}</span>
+      </div>
+    </div>
+  );
 };
 
 export default ThreadBox;
