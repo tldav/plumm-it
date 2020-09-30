8272234f3a8dbb9b7e2bf9ef7ac412a9d70c5d81
Neverender <tldavis09@yahoo.com>
2020-09-29 20:23:15 -0500

    There's working routes to get all threads and to retreive a single thread, as well as all of the comments for that thread. Not sure what the best way to handle the nested child comments is. As of now, finding a thread by id returns a large array of all comments, regardless of parent/child relationship

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const { connect } = require("../routes");
 
 const connection = mysql.createConnection({
 	host: "localhost",
@@ -27,22 +28,84 @@ const db = {
 	},
 	allThreads: () => {
 		return new Promise((resolve, reject) => {
-			connection.query(`SELECT * FROM threads`, (err, results) => {
-				if (err) {
-					return reject(err);
+			connection.query(
+				`SELECT t.thread_id, t.title, t.body, t.created_at, t.upvotes, t.downvotes, u.username 
+                    FROM threads t
+                    JOIN users u
+                    ON t.user_id = u.user_id;`,
+				(err, results) => {
+					if (err) {
+						return reject(err);
+					}
+					return resolve(results);
 				}
-				return resolve(results);
-			});
+			);
 		});
 	},
 	allComments: () => {
 		return new Promise((resolve, reject) => {
-			connection.query(`SELECT * FROM comments`, (err, results) => {
-				if (err) {
-					return reject(err);
+			connection.query(
+				`SELECT * 
+                    FROM threads t
+                    JOIN comments c
+                    ON t.thread_id = c.thread_id;`,
+				(err, results) => {
+					if (err) {
+						return reject(err);
+					}
+					return resolve(results);
 				}
-				return resolve(results);
-			});
+			);
+		});
+	},
+	oneThread: (id) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				`SELECT t.thread_id, t.title, t.body, t.created_at, t.upvotes, t.downvotes, u.username 
+                    FROM threads t
+                    JOIN users u
+                    ON t.user_id = u.user_id
+                    WHERE t.thread_id = ${id};`,
+				(err, results) => {
+					if (err) {
+						return reject(err);
+					}
+					return resolve(results);
+				}
+			);
+		});
+	},
+	threadComments: (id) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				`SELECT c.comment_id, c.parent_comment_id, c.body, c.created_at, c.upvotes, c.downvotes, u.username
+                FROM comments c
+                JOIN threads t ON t.thread_id = c.thread_id
+                JOIN users u ON c.user_id = u.user_id
+                WHERE t.thread_id = ${id};`,
+				(err, results) => {
+					if (err) {
+						return reject(err);
+					}
+					return resolve(results);
+				}
+			);
+		});
+	},
+	childComments: (id) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				`SELECT *
+            FROM comments cc
+            JOIN comments pc ON pc.parent_comment_id = cc.comment_id
+            WHERE cc.comment_id = ${id};`,
+				(err, results) => {
+					if (err) {
+						return reject(err);
+					}
+					return resolve(results);
+				}
+			);
 		});
 	}
 };
--- server/schema/schema.sql
+++ server/schema/schema.sql
@@ -3,41 +3,65 @@ CREATE DATABASE plumm_db;
 
 USE plumm_db;
 
-CREATE TABLE users(
-	user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    email VARCHAR(320) NOT NULL UNIQUE,
-    username VARCHAR(30) NOT NULL UNIQUE,
-    first_name VARCHAR(50) NOT NULL, 
-    last_name VARCHAR (50) NOT NULL,
-    user_password VARCHAR(250) NOT NULL,
+CREATE TABLE users
+(
+    user_id INT
+    UNSIGNED NOT NULL AUTO_INCREMENT,
+    email VARCHAR
+    (320) NOT NULL UNIQUE,
+    username VARCHAR
+    (30) NOT NULL UNIQUE,
+    first_name VARCHAR
+    (50) NOT NULL, 
+    last_name VARCHAR
+    (50) NOT NULL,
+    user_password VARCHAR
+    (250) NOT NULL,
     created_at DATETIME NOT NULL,
-    PRIMARY KEY (user_id)
+    PRIMARY KEY
+    (user_id)
 );
 
-CREATE TABLE threads(
-	thread_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    CREATE TABLE threads
+    (
+        thread_id INT
+        UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(200) NOT NULL,
+    title VARCHAR
+        (200) NOT NULL,
     body TEXT NOT NULL,
     created_at DATETIME NOT NULL,
     upvotes INT DEFAULT 0,
     downvotes INT DEFAULT 0,
-    FOREIGN KEY(user_id) REFERENCES users(user_id),
-    PRIMARY KEY(thread_id)
+    FOREIGN KEY
+        (user_id) REFERENCES users
+        (user_id),
+    PRIMARY KEY
+        (thread_id)
 );
 
-CREATE TABLE comments(
-	comment_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        CREATE TABLE comments
+        (
+            comment_id INT
+            UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     thread_id INT UNSIGNED NOT NULL,
-    parent_comment_id INT UNSIGNED NOT NULL,
-    active NUMBER(1),
+    parent_comment_id INT UNSIGNED,
+    is_active TINYINT
+            (1),
     body TEXT NOT NULL,
     created_at DATETIME NOT NULL,
     upvotes INT DEFAULT 0,
     downvotes INT DEFAULT 0,
-    FOREIGN KEY(user_id) REFERENCES users(user_id),
-    FOREIGN KEY(thread_id) REFERENCES threads(thread_id),
-    FOREIGN KEY(parent_comment_id) REFERENCES comments(comment_id),
-    PRIMARY KEY (comment_id)
+    FOREIGN KEY
+            (user_id) REFERENCES users
+            (user_id),
+    FOREIGN KEY
+            (thread_id) REFERENCES threads
+            (thread_id),
+    FOREIGN KEY
+            (parent_comment_id) REFERENCES comments
+            (comment_id),
+    PRIMARY KEY
+            (comment_id)
 );
