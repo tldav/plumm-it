72a0c6a41adf86632017832cc1397080f1375d31
Neverender <tldavis09@yahoo.com>
2021-03-31 20:07:19 -0500

    .

--- server/config/index.js
+++ server/config/index.js
@@ -10,9 +10,7 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-	if (err) {
-		console.log(err);
-	}
+	if (err) console.log(err);
 });
 
 module.exports = connection;
