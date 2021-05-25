e9ace4a6f1a4057ba4f6211efcc92058a7719ade
nick wuerz <nawuerz@gmail.com>
2021-05-24 21:46:58 -0500

    updates style / adds data from db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Switch, withRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import "./stylesheets/App.css";
-import FeaturedThreadBox from "./components/FeaturedThreadBox";
+import Thread from "./pages/Thread";
 
 const App = ({ location }) => {
   const { pathname } = location;
@@ -15,16 +15,10 @@ const App = ({ location }) => {
         <Route exact path={["/", "/home"]}>
           <Home />
         </Route>
-        <Route exact path={pathname} component={FeaturedThreadBox} />
+        <Route exact path={pathname} component={Thread} />
       </Switch>
     </>
   );
-};
-
-const mapStateToProps = (state) => {
-  return {
-    threadRoute: state.featuredThread.threadRoute,
-  };
 };
 
 export default withRouter(App);
