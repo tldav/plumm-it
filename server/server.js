84232c4b69bc829353b54c0f106a607632d338cc
Neverender <tldavis09@yahoo.com>
2021-05-29 19:52:35 -0500

    add express-session config to serverjs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const session = require("express-session");
 const logger = require("morgan");
+const MySQLStore = require("express-mysql-session")(session);
+require("dotenv").config();
 const routes = require("./routes");
+const connection = require("./config").database;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,10 +18,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 
+// express-session middleware
+const sessionStore = new MySQLStore(connection);
+
+app.use(
+	session({
+		key: "my_cookie",
+		secret: process.env.SESSION_SECRET,
+		store: sessionStore,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 24,
+		},
+	})
+);
+
 // Serve static assets
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
+
+app.use((req, res, next) => {
+	console.log(req.session);
+	next();
+});
 
 app.use("/api", routes);
 
