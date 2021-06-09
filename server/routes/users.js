a70086a771ac7be24d45dff8f97d291b6b337727
Neverender <tldavis09@yahoo.com>
2021-06-09 17:27:24 -0500

    add routes for user authentication and logout. Need to finish registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,7 @@
 // Users Routes
 const router = require("express").Router();
+const passport = require("passport");
+const bcrypt = require("bcrypt");
 const User = require("../models").User;
 
 router.get("/", async (req, res) => {
@@ -11,14 +13,15 @@ router.get("/", async (req, res) => {
 	}
 });
 
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
 	const { email, username, firstName, lastName, password } = req.body;
-	// *add await to hashedPassword if the line below breaks*
-	const hashedPassword = bcrypt.hashSync(password, 10);
+
 	try {
+		const hashedPassword = await bcrypt.hash(password, 10);
 		const newUser = await User.create(email, username, firstName, lastName, hashedPassword);
 
 		res.json(newUser);
+		// res.redirect("/login");
 	} catch (err) {
 		res.status(500).json(err);
 	}
@@ -34,6 +37,38 @@ router.put("/:id", async (req, res) => {
 	} catch (err) {
 		res.status(500).json(err);
 	}
+});
+
+// LOGIN USER &  SEND USER DATA
+router.post("/login", passport.authenticate("local"), (req, res) => {
+	const { user_id, email, username, first_name, last_name, created_at, is_active } = req.user;
+
+	res.json({ user_id, email, username, first_name, last_name, created_at, is_active });
+});
+
+// ALT LOGIN ATTEMPT
+// router.post(
+// 	"/login",
+// 	passport.authenticate("local", {
+// 		failureRedirect: "login-faiure",
+// 		successRedirect: "login-success",
+// 	})
+// );
+
+router.get("/login-failure", (req, res) => {
+	res.send("Incorrect password. Please try again.");
+	console.log("Incorrect password. Please try again.");
+});
+
+router.get("/login-success", (req, res) => {
+	res.send("You have been successfully logged in");
+	console.log("You have been successfully logged in");
+});
+
+router.get("/logout", (req, res) => {
+	req.logout();
+	res.json("Successfully logged out");
+	console.log("***req.user from logout route", req.user);
 });
 
 module.exports = router;
