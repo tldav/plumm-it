d2341971041223deff79997aa8a2b3af91ab867f
tldav <tldavis09@yahoo.com>
2021-08-24 17:42:16 -0500

    add API call to header title click to ensure render on browser back button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import UserDialog from "./UserDialog"
 import "../stylesheets/Header.css";
+import { ThreadContext } from "../context/ThreadContext";
 
 const Header = () => {
   let history = useHistory();
+  const { handleReturnHome } = useContext(ThreadContext)
+
+  const onHomeClick = () => {
+    handleReturnHome()
+    history.push("/")
+  }
+
   return (
     <div className="header">
       <div className="header-flex-container">
-        <h1 id="plumm-title" onClick={() => history.push("/")}>
+        <h1 id="plumm-title" onClick={onHomeClick}>
           plummit
         </h1>
         <div className="header-buttons">
-            <UserDialog purpose="signup" buttonTheme="dialog-button-2"/>
+            <UserDialog purpose="signup" buttonTheme="no-style-button"/>
             <UserDialog purpose="login" buttonTheme="dialog-button-2"/>
         </div>
       </div>
